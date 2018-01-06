c58afe282d4368c1c48be66692600080baa8abef
Kate McFaul <katemcfaul@gmail.com>
2018-01-06 15:38:26 -0800

    Add header

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+
 import './App.css';
+import Header from '../Header/Header';
+
 
 class App extends Component {
   render() {
     return (
-      <p>Hello World</p>
+      <Header />
     );
   }
 }
