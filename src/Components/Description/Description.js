e46f7b91c6aef9b2258558d56754e391f0beb222
Kate McFaul <katemcfaul@gmail.com>
2018-01-07 20:40:39 -0800

    update text

--- src/Components/Description/Description.js
+++ src/Components/Description/Description.js
@@ -4,10 +4,10 @@ import './Description.css';
 const Description = (props) => (
   <div className="description">
     <p className="title">What to do:</p>
-    <p>Is your guage a little off or the size of your item not quite right? <span>Enter your current guage and the needle or hook you used</span> then:</p>
+    <p>Is your gauge a little off or the size of your item not quite right? <span>Enter your current gauge and the needle or hook you used</span> then:</p>
     <ul>
-      <li>- <span>Enter what your guage should be</span> and click calculate to see what needle you should use</li>
-  <li>- Or <span>enter which needle you'd like to use</span> to see what your guage will be!</li>
+      <li>- <span>Enter what your gauge should be</span> and click calculate to see what needle you should use</li>
+  <li>- Or <span>enter which needle you'd like to use</span> to see what your gauge will be!</li>
     </ul>
     <p 
       className="title"
@@ -15,8 +15,8 @@ const Description = (props) => (
       Tips:
     </p>
     <ul>
-      <li>- Whether you're calculating guage or length or width or circumference it all works the same. Whatever unit you start with is the unit your results will be in.</li>
-      <li>- To enter a guage of 6 sts per 1" or 6 sts per 4" or a length of 6 cm, just enter a 6.</li>
+      <li>- Whether you're calculating gauge or length or width or circumference it all works the same. Whatever unit you start with is the unit your results will be in.</li>
+      <li>- To enter a gauge of 6 sts per 1" or 6 sts per 4" or a length of 6 cm, just enter a 6.</li>
       <li>- Only three fields may be entered at a time. Erase one of the last two to change which gets calculated</li>
       <li>- Fractions okay! Enter them with a space between whole number and fraction like: 4 5/8</li>
     </ul>
