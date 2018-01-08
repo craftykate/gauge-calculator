cec10d6f3d5d55f97b8e836ec6c7d770ce4d3fd1
Kate McFaul <katemcfaul@gmail.com>
2018-01-07 19:21:00 -0800

    delete trailing zeros

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -4,12 +4,12 @@ import calculations from '../../utils/calculations';
 const results = (props) => (
   <div className="result">
     <p className="title">What it will be:</p>
-    <p>Using a <span style={{ fontWeight: 'bold' }}>{props.state.willBeNeedle}</span> mm needle/hook your item will be <span style={{ fontWeight: 'bold' }}>{props.state.willBeBig}</span> units big</p>
+    <p>Using a <span style={{ fontWeight: 'bold' }}>{props.state.willBeNeedle}</span> mm needle/hook your item will be <span style={{ fontWeight: 'bold' }}>{Number(props.state.willBeBig).toString()}</span> units big</p>
     
     {props.state.willBeNeedle1 ?
       <React.Fragment>
-        <p>Using a <span style={{ fontWeight: 'bold' }}>{props.state.willBeNeedle1}</span> mm needle/hook your item will be <span style={{ fontWeight: 'bold' }}>{props.state.willBeBig1}</span> units big</p>
-        <p style={{ fontStyle: 'italic' }}>Target size: {calculations.undoFraction(props.state.shouldBig)} units</p>
+        <p>Using a <span style={{ fontWeight: 'bold' }}>{props.state.willBeNeedle1}</span> mm needle/hook your item will be <span style={{ fontWeight: 'bold' }}>{Number(props.state.willBeBig1).toString()}</span> units big</p>
+        <p style={{ fontStyle: 'italic' }}>Target size: {Number(calculations.undoFraction(props.state.shouldBig)).toString()} units</p>
       </React.Fragment>
       : null
     }
